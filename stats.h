da87063aed8607609f063c2072a6368ead4fc685
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-01-28 00:20:05 +0530

    Update stats.h
    Declared the Email alert and LED alert functions

--- stats.h
+++ stats.h
@@ -8,6 +8,9 @@ struct Stats
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
+void emailAlerter();
+void ledAlerter();
+
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
 extern int emailAlertCallCount;
