addbec72da811694e8f088ebe659677109458732
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-01-28 01:22:30 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -23,7 +23,7 @@ TEST_CASE("average is NaN for empty array")
 {
     Stats computedStats = compute_statistics(0, 0);
    
-    REQUIRE((computedStats.average)!=(computedStats.average));
+    REQUIRE((isnan(computedStats.average))==1);
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
